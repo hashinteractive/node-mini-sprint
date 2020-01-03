204e63af77cdbda752d89abe1910131396a87844
tthenne <troy@hashinteractive.com>
2020-01-03 12:26:10 -0700

    finished mini sprint

--- client/main.js
+++ client/main.js
@@ -1,4 +1,31 @@
 $(document).ready(function() {
+  const endpoint = '//localhost:3333'
+
+  function getQuote(){
+    $.get(`${endpoint}/quote`).done( response => {
+      $('#quote').text(response)
+    }).fail(err => {
+      console.log(err)
+    })
+  }
+
+  function addQuote(quote){
+    let data = { quote: quote } 
+    //YOUR CODE HERE, Add a POST request
+    $.ajax({
+      url: `${endpoint}/quote`,
+      type: "POST",
+      data: JSON.stringify(data),
+      contentType:"application/json; charset=utf-8",
+      dataType:"json",
+    }).done( response => {
+      const { quote } = response
+      $('input').val('');
+      $('#quote').text(quote)
+    }).fail(err => {
+      console.log(err)
+    })
+  }
 
 // get a quote from the server when the page loads and add it to the dom
   getQuote();
@@ -10,15 +37,4 @@ $(document).ready(function() {
     addQuote(quote);
   });
 
-  function getQuote(){
-
-    //YOUR CODE HERE, Add a GET request
-
-  }
-
-  function addQuote(quote){
-    
-    //YOUR CODE HERE, Add a POST request
-
-  }
 });
--- server/index.js
+++ server/index.js
@@ -8,15 +8,15 @@ const headers = {
   "access-control-max-age": 10
 };
 
-const port = 3000;
+const port = 3333;
 
 // TODO: Fill with strings of your favorite quotes :)
 const quotes = [
-  'one',
-  'two',
-  'three',
-  'four',
-  'five'
+  'Compassion has no limit. Kindness has no enemy.',
+  'Love has no end, only a beginning.',
+  'Empty yourself and let the universe fill you.',
+  'Life is a chance. Love is infinity. Grace is reality.',
+  'Work but don’t forget to live.'
 ];
 
 //Utility Function to return a random integer
@@ -27,30 +27,59 @@ function getRandomInt(min, max) {
 }
 
 const handleRequest = function(req, res) {
-  console.log(`Endpoint: ${req.url} Method: ${req.method}`);
+  //console.log(`Endpoint: ${req.url} Method: ${req.method}`);
+  for(header in headers){
+    res.setHeader(header, headers[header])
+  } 
+	if ( req.method === 'OPTIONS' ) {
+		res.writeHead(200);
+		res.end();
+		return;
+	}
 
   // redirect users to /quote if they try to hit the homepage. This should already work, no changes needed
   if (req.url == '/') {
     console.log('redirecting');
     res.writeHead(301, {...headers, Location: `http://localhost:${port}/quote`}) //redirect to quote
     res.end();
+    return;
   }
 
-  // TODO: GET ONE
-  if ((req.url == '/quote/' || req.url == '/quote') && req.method == "FILL ME IN") {
+  if ((req.url == '/quotes/' || req.url == '/quotes') && req.method == "GET") {
     //YOUR CODE HERE
-
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(quotes))
+    return;
+  }
+  // TODO: GET ONE
+  else if ((req.url == '/quote/' || req.url == '/quote') && req.method == "GET") {
+    //YOUR CODE HERE
+    const quote = quotes[getRandomInt(0, quotes.length)]
+    res.writeHead(200, { 'Content-Type': 'text/plain' })
+    res.end(quote)
+    return;
   }
   // TODO: POST/CREATE
-  else if ((req.url == 'FILL ME IN' || req.url == 'FILL ME IN') && req.method == "FILL ME IN") {
+  else if ((req.url == '/quote/' || req.url == '/quote') && req.method == "POST") {
     //YOUR CODE HERE
+    let body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    }).on('end', () => {
+      body = JSON.parse(Buffer.concat(body).toString());
+      // at this point, `body` has the entire request body stored in it as a string
+      let { quote } = body
+      quotes.push(quote)
+      res.writeHead(200, { 'Content-Type': 'text/plain' })
+      res.end(JSON.stringify(body))
+    });
   }
 
 //CATCH ALL ROUTE
   else {
     res.writeHead(404,headers);
     res.end('Page not found');
-
+    return;
   }
 }
 
